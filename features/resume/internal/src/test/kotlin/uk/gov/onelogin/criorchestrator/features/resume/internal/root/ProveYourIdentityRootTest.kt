81f46a305924a2ca02b5c33565f6162414d02646
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-02-21 11:42:22 +0000

    feat: Add continue to prove identity screen UI

--- features/resume/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/root/ProveYourIdentityRootTest.kt
+++ features/resume/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/root/ProveYourIdentityRootTest.kt
@@ -43,7 +43,10 @@ class ProveYourIdentityRootTest {
 
     private val card: SemanticsMatcher = hasTestTag(ProveYourIdentityRootTestTags.CARD)
     private val modal: SemanticsMatcher = hasTestTag(ProveYourIdentityRootTestTags.MODAL)
-    private val closeButton = hasContentDescription("Close")
+
+    // FIXME This content description should be "Cancel button"
+    //  https://govukverify.atlassian.net/browse/DCMAW-12003
+    private val closeButton = hasContentDescription("Close Button")
     private val continueButton = hasText(context.getString(R.string.start_id_check_primary_button))
 
     @Test
