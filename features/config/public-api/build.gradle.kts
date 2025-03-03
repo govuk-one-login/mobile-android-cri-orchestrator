fc9f6359064f78f99fc1ec5e307ce348bef45348
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-03-03 15:12:56 +0000

    refactor: Update structure of config feature

--- features/config/public-api/build.gradle.kts
+++ features/config/public-api/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     id("uk.gov.onelogin.criorchestrator.android-lib-config")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 dependencies {
+    api(libs.kotlinx.collections.immutable)
+
     implementation(libs.kotlinx.coroutines)
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.uk.gov.logging.api)
     implementation(project(":libraries:di"))
 
