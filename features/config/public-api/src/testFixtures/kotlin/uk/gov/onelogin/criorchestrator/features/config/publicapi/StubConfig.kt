ea9c7b54009c8f4c26b6e2ab12992c848a1be058
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-03-03 15:12:56 +0000

    refactor: Update structure of config feature

--- features/config/public-api/src/testFixtures/kotlin/uk/gov/onelogin/criorchestrator/features/config/publicapi/StubConfig.kt
+++ features/config/public-api/src/testFixtures/kotlin/uk/gov/onelogin/criorchestrator/features/config/publicapi/StubConfig.kt
@@ -1,13 +1,38 @@
 package uk.gov.onelogin.criorchestrator.features.config.publicapi
 
-/**
- * Convenience object that provides a dummy value for the backend asynchronous base URL.
- */
-object StubConfig {
-    const val BACKEND_ASYNC_URL_TEST_VALUE = "https://test.backend.url"
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
-    fun provideConfig() =
-        ConfigProvider(
-            backendAsyncUrl = BACKEND_ASYNC_URL_TEST_VALUE,
-        )
-}
+data object StubStringConfigKey : ConfigKey<Config.Value.StringValue>(
+    name = "stub string config key",
+)
+
+data object StubBooleanConfigKey : ConfigKey<Config.Value.BooleanValue>(
+    name = "stub boolean config key",
+)
+
+fun stubStringConfigEntry(
+    key: ConfigKey<Config.Value.StringValue> = StubStringConfigKey,
+    value: String = "stub string config value",
+) = Config.Entry<Config.Value.StringValue>(
+    key = key,
+    value = Config.Value.StringValue(value = value),
+)
+
+fun stubBooleanConfigEntry(
+    key: ConfigKey<Config.Value.BooleanValue> = StubBooleanConfigKey,
+    value: Boolean = true,
+) = Config.Entry<Config.Value.BooleanValue>(
+    key = key,
+    value = Config.Value.BooleanValue(value = value),
+)
+
+fun stubConfig(
+    entries: ImmutableList<Config.Entry<Config.Value>> =
+        persistentListOf(
+            stubStringConfigEntry(),
+            stubBooleanConfigEntry(),
+        ),
+) = Config(
+    entries = entries,
+)
