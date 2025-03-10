47ca78ce11b3477999f87dfe80b267aaf415b19a
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2025-03-10 15:15:15 +0000

    build: Apply publishing workaround project-wide (#175)

--- build-logic/plugins/src/main/kotlin/uk/gov/onelogin/criorchestrator/kotlin-lib-config.gradle.kts
+++ build-logic/plugins/src/main/kotlin/uk/gov/onelogin/criorchestrator/kotlin-lib-config.gradle.kts
@@ -1,6 +1,7 @@
 package uk.gov.onelogin.criorchestrator
 
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import uk.gov.onelogin.criorchestrator.extensions.disableJavadocGeneration
 import uk.gov.onelogin.criorchestrator.extensions.setJavaVersion
 import kotlin.collections.forEach
 import kotlin.collections.listOf
@@ -12,6 +13,10 @@ listOf(
 ).forEach {
     project.plugins.apply(it)
 }
+
+// https://govukverify.atlassian.net/browse/DCMAW-11888
+// https://github.com/Kotlin/dokka/issues/2956
+project.disableJavadocGeneration()
 
 configure<KotlinJvmProjectExtension> {
     setJavaVersion()
