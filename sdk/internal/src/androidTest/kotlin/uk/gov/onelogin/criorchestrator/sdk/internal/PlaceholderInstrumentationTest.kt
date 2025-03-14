71f76e22e969b1151f6382192fd3b3d2313a0498
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-03-14 12:08:26 +0000

    docs: Add reference to test bug in code comments

--- sdk/internal/src/androidTest/kotlin/uk/gov/onelogin/criorchestrator/sdk/internal/PlaceholderInstrumentationTest.kt
+++ sdk/internal/src/androidTest/kotlin/uk/gov/onelogin/criorchestrator/sdk/internal/PlaceholderInstrumentationTest.kt
@@ -9,6 +9,8 @@ import org.junit.runner.RunWith
  * This test is needed due to a bug in `mobile-android-pipelines` requiring
  * all modules reporting test coverage to have at least one test in each of
  * `test` and `androidTest` source sets.
+ *
+ * This will be addressed in [DCMAW-11855](https://govukverify.atlassian.net/browse/DCMAW-11855)
  */
 @RunWith(AndroidJUnit4::class)
 class PlaceholderInstrumentationTest {
