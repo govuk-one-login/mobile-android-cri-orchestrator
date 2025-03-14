7f0ab74d167218049bba8177dc98cee0fe52d3fc
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-03-14 12:03:44 +0000

    chore: address additional code review comments

--- features/select-doc/internal-api/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internalapi/nav/SelectDocumentDestinations.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.onelogin.criorchestrator.features.selectdoc.internalapi.nav
-
-import kotlinx.serialization.Serializable
-import uk.gov.onelogin.criorchestrator.libraries.navigation.NavigationDestination
-
-sealed interface SelectDocumentDestinations : NavigationDestination {
-    @Serializable
-    data object SelectDocument : SelectDocumentDestinations
-}
