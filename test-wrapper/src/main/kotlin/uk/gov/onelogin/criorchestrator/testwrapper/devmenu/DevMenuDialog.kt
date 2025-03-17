89b1fb7d7bab06185d6f803daeec45185777342b
JacksonJ2W <152998610+JacksonJ2W@users.noreply.github.com>
2025-03-17 10:58:53 +0000

    fix: update pages to patterns in FullScreenDialog import path (#185)

--- test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenuDialog.kt
+++ test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenuDialog.kt
@@ -3,7 +3,7 @@ package uk.gov.onelogin.criorchestrator.testwrapper.devmenu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import uk.gov.android.ui.pages.dialog.FullScreenDialog
+import uk.gov.android.ui.patterns.dialog.FullScreenDialog
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.onelogin.criorchestrator.features.dev.publicapi.DevMenuScreen
 import uk.gov.onelogin.criorchestrator.sdk.sharedapi.CriOrchestratorComponent
