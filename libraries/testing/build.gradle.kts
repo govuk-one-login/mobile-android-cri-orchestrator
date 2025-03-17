37e1402036e8ac97b0820a588e8078f40bac7279
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-03-17 17:08:01 +0000

    test: Add test utility for reporting analytics tests

--- libraries/testing/build.gradle.kts
+++ libraries/testing/build.gradle.kts
@@ -6,6 +6,8 @@ dependencies {
     testFixturesImplementation(platform(libs.org.junit.bom))
     testFixturesImplementation(libs.org.junit.jupiter.api)
     testFixturesImplementation(libs.kotlinx.coroutines.test)
+    testFixturesImplementation(libs.org.junit.junit4)
+    testFixturesImplementation(libs.uk.gov.logging.api)
 }
 
 mavenPublishingConfig {
