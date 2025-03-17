845a402a920bdb373927d8d3631adf4bb6ce6165
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-03-17 16:50:26 +0000

    fix: lint issues

--- features/session/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/FakeSessionApiTest.kt
+++ features/session/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/FakeSessionApiTest.kt
@@ -1,8 +1,8 @@
 package uk.gov.onelogin.criorchestrator.features.session.internal
 
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import uk.gov.android.network.api.ApiResponse
 import uk.gov.onelogin.criorchestrator.features.session.internal.network.FakeSessionApi
 
@@ -13,14 +13,14 @@ class FakeSessionApiTest {
             assertEquals(
                 ApiResponse.Success<String>(
                     """
-                {
-                    "sessionId": "test session ID",
-                    "redirectUri": "https://example/redirect",
-                    "state": "11112222333344445555666677778888"
-                }
-                """.trimIndent(),
+                    {
+                        "sessionId": "test session ID",
+                        "redirectUri": "https://example/redirect",
+                        "state": "11112222333344445555666677778888"
+                    }
+                    """.trimIndent(),
                 ),
-                FakeSessionApi().getActiveSession()
+                FakeSessionApi().getActiveSession(),
             )
         }
 }
