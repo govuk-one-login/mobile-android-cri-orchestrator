fa8446a3788936420288e8b75c53e32892c560d0
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-03-18 15:27:25 +0000

    feat: Enable screen tracking

--- features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/analytics/ResumeAnalytics.kt
+++ features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/analytics/ResumeAnalytics.kt
@@ -7,6 +7,7 @@ import uk.gov.logging.api.analytics.parameters.data.TaxonomyLevel3
 import uk.gov.logging.api.v3dot1.logger.logEventV3Dot1
 import uk.gov.logging.api.v3dot1.model.RequiredParameters
 import uk.gov.logging.api.v3dot1.model.TrackEvent
+import uk.gov.logging.api.v3dot1.model.ViewEvent
 import uk.gov.onelogin.criorchestrator.libraries.androidutils.resources.ResourceProvider
 import javax.inject.Inject
 
@@ -27,6 +28,17 @@ class ResumeAnalytics
         ) = analyticsLogger.logEventV3Dot1(
             TrackEvent.Button(
                 text = resourceProvider.getEnglishString(buttonText),
+                params = requiredParameters,
+            ),
+        )
+
+        fun trackScreen(
+            id: ScreenId,
+            @StringRes title: Int,
+        ) = analyticsLogger.logEventV3Dot1(
+            ViewEvent.Screen(
+                id = id.rawId,
+                name = resourceProvider.getEnglishString(title),
                 params = requiredParameters,
             ),
         )
