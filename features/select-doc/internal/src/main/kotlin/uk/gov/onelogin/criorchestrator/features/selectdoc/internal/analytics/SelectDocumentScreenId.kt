27001483b689622c51a2cc0355e3de9a809db7c1
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-03-19 17:00:46 +0000

    feat: add ViewModel for SelectDocumentScreen

---
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/analytics/SelectDocumentScreenId.kt
@@ -0,0 +1,9 @@
+package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.analytics
+
+import uk.gov.onelogin.criorchestrator.libraries.analytics.ScreenId
+
+enum class SelectDocumentScreenId(
+    override val rawId: String,
+): ScreenId {
+    SelectPassport(rawId = "ccb1caa0-0331-4122-9f35-78ce28e0b4b5"),
+}
