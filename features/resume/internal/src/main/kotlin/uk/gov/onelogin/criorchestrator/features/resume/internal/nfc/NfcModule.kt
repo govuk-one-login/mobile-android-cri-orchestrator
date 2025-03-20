2ec6b74a4f7d83f4360895462c0ca535c8590cb3
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-03-20 09:30:42 +0000

    fix: add credentials to github workflows

--- features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/nfc/NfcModule.kt
+++ features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/nfc/NfcModule.kt
@@ -2,23 +2,17 @@ package uk.gov.onelogin.criorchestrator.features.resume.internal.nfc
 
 import android.content.Context
 import android.nfc.NfcManager
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
 import com.squareup.anvil.annotations.ContributesTo
 import dagger.Module
 import dagger.Provides
 import uk.gov.idcheck.sdk.passport.nfc.checker.NfcChecker
 import uk.gov.idcheck.sdk.passport.nfc.checker.NfcCheckerImpl
-import uk.gov.onelogin.criorchestrator.features.resume.internal.analytics.ResumeAnalytics
 import uk.gov.onelogin.criorchestrator.libraries.di.ActivityScope
 import uk.gov.onelogin.criorchestrator.libraries.di.CriOrchestratorScope
-import javax.inject.Named
 
 @Module
 @ContributesTo(CriOrchestratorScope::class)
 object NfcModule {
-
     @Provides
     @ActivityScope
     fun providesNfcManager(context: Context): NfcManager = context.getSystemService(Context.NFC_SERVICE) as NfcManager
