4e9356e347bd956d9ca9238b59077e1f287a5157
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-03-20 08:58:54 +0000

    refactor: move nfcc checker to a new module

--- features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/screen/ContinueToProveYourIdentityViewModelModule.kt
+++ features/resume/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/resume/internal/screen/ContinueToProveYourIdentityViewModelModule.kt
@@ -1,7 +1,5 @@
 package uk.gov.onelogin.criorchestrator.features.resume.internal.screen
 
-import android.content.Context
-import android.nfc.NfcManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -9,9 +7,7 @@ import com.squareup.anvil.annotations.ContributesTo
 import dagger.Module
 import dagger.Provides
 import uk.gov.idcheck.sdk.passport.nfc.checker.NfcChecker
-import uk.gov.idcheck.sdk.passport.nfc.checker.NfcCheckerImpl
 import uk.gov.onelogin.criorchestrator.features.resume.internal.analytics.ResumeAnalytics
-import uk.gov.onelogin.criorchestrator.libraries.di.ActivityScope
 import uk.gov.onelogin.criorchestrator.libraries.di.CriOrchestratorScope
 import javax.inject.Named
 
@@ -19,14 +15,6 @@ import javax.inject.Named
 @ContributesTo(CriOrchestratorScope::class)
 object ContinueToProveYourIdentityViewModelModule {
     const val FACTORY_NAME = "ContinueToProveYourIdentityViewModelFactory"
-
-    @Provides
-    @ActivityScope
-    fun providesNfcManager(context: Context): NfcManager = context.getSystemService(Context.NFC_SERVICE) as NfcManager
-
-    @Provides
-    @ActivityScope
-    fun provideNfcChecker(nfcManager: NfcManager): NfcChecker = NfcCheckerImpl(nfcManager)
 
     @Provides
     @Named(FACTORY_NAME)
