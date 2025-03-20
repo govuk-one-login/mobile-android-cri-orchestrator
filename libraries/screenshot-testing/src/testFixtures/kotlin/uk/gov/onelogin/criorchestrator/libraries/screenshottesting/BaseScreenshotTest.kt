362bff3e40b21d1d36c2b5be83b95e1c725e2090
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-03-20 15:17:19 +0000

    test: Align screenshot test size with Compose previews

--- libraries/screenshot-testing/src/testFixtures/kotlin/uk/gov/onelogin/criorchestrator/libraries/screenshottesting/BaseScreenshotTest.kt
+++ libraries/screenshot-testing/src/testFixtures/kotlin/uk/gov/onelogin/criorchestrator/libraries/screenshottesting/BaseScreenshotTest.kt
@@ -3,12 +3,17 @@ package uk.gov.onelogin.criorchestrator.libraries.screenshottesting
 import android.content.res.Configuration
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
+import com.android.resources.Density
 import com.android.resources.NightMode
 import org.junit.Rule
 import org.junit.Test
 import sergio.sastre.composable.preview.scanner.android.AndroidPreviewInfo
+import sergio.sastre.composable.preview.scanner.android.device.DevicePreviewInfoParser
 import sergio.sastre.composable.preview.scanner.android.screenshotid.AndroidPreviewScreenshotIdBuilder
 import sergio.sastre.composable.preview.scanner.core.preview.ComposablePreview
+import kotlin.math.ceil
+
+private const val BASELINE_DPI = 160f
 
 /**
  * A base class to execute screenshot tests.
@@ -48,15 +53,35 @@ abstract class BaseScreenshotTest(
     }
 
     private fun createPaparazziRule(preview: ComposablePreview<AndroidPreviewInfo>): Paparazzi {
+        val preview = preview.previewInfo
         val nightMode =
-            when (preview.previewInfo.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
+            when (preview.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
                 true -> NightMode.NIGHT
                 false -> NightMode.NOTNIGHT
             }
-        val locale = preview.previewInfo.locale
+        val locale = preview.locale
+        val parsedDevice =
+            DevicePreviewInfoParser.parse(preview.device)?.inPx()
+                ?: error("Invalid device ${preview.device}")
+        val conversionFactor = parsedDevice.densityDpi / BASELINE_DPI
+        val previewWidthInPx = ceil(preview.widthDp * conversionFactor).toInt()
+        val previewHeightInPx = ceil(preview.heightDp * conversionFactor).toInt()
         return Paparazzi(
             deviceConfig =
                 DeviceConfig(
+                    density = Density(parsedDevice.densityDpi),
+                    screenHeight =
+                        when (preview.heightDp > 0) {
+                            true -> previewHeightInPx
+                            false -> parsedDevice.dimensions.height.toInt()
+                        },
+                    screenWidth =
+                        when (preview.widthDp > 0) {
+                            true -> previewWidthInPx
+                            false -> parsedDevice.dimensions.width.toInt()
+                        },
+                    xdpi = parsedDevice.densityDpi, // not 100% precise
+                    ydpi = parsedDevice.densityDpi,
                     nightMode = nightMode,
                     locale = locale.ifEmpty { null },
                 ),
