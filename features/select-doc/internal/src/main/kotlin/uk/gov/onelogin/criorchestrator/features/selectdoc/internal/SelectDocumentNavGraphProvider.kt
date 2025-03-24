df010c701bd63ef0636af30bbb81e5ee81c0f9b4
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-03-24 14:18:44 +0000

    fix: add select passport view model module

--- features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/SelectDocumentNavGraphProvider.kt
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/SelectDocumentNavGraphProvider.kt
@@ -1,31 +1,34 @@
 package uk.gov.onelogin.criorchestrator.features.selectdoc.internal
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.squareup.anvil.annotations.ContributesMultibinding
 import uk.gov.onelogin.criorchestrator.features.resume.internalapi.nav.ProveYourIdentityNavGraphProvider
 import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.passport.SelectPassportScreen
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.passport.SelectPassportViewModelModule
 import uk.gov.onelogin.criorchestrator.features.selectdoc.internalapi.nav.SelectDocumentDestinations
 import uk.gov.onelogin.criorchestrator.libraries.di.CriOrchestratorScope
 import javax.inject.Inject
+import javax.inject.Named
 
 @ContributesMultibinding(CriOrchestratorScope::class)
 class SelectDocumentNavGraphProvider
     @Inject
-    constructor() : ProveYourIdentityNavGraphProvider {
+    constructor(
+        @Named(SelectPassportViewModelModule.FACTORY_NAME)
+        private val viewModelFactory: ViewModelProvider.Factory,
+    ) : ProveYourIdentityNavGraphProvider {
         override fun NavGraphBuilder.contributeToGraph(navController: NavController) {
             composable<SelectDocumentDestinations.Passport> {
-                SelectPassportScreen()
+                SelectPassportScreen(
+                    viewModel = viewModel(factory = viewModelFactory),
+                )
             }
 
             composable<SelectDocumentDestinations.DrivingLicence> {
-                @Composable
-                fun Test() {
-                    Text("Driving License check")
-                }
             }
         }
     }
