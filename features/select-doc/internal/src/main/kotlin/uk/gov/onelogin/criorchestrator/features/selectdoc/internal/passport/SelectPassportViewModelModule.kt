a98218640701b3693c11d41ef7d345cad40b50a0
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-03-24 14:18:44 +0000

    fix: add select passport view model module

---
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/passport/SelectPassportViewModelModule.kt
@@ -0,0 +1,26 @@
+package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.passport
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.squareup.anvil.annotations.ContributesTo
+import dagger.Module
+import dagger.Provides
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.analytics.SelectDocumentAnalytics
+import uk.gov.onelogin.criorchestrator.libraries.di.CriOrchestratorScope
+import javax.inject.Named
+
+@Module
+@ContributesTo(CriOrchestratorScope::class)
+object SelectPassportViewModelModule {
+    const val FACTORY_NAME = "SelectPassportViewModelModuleFactory"
+
+    @Provides
+    @Named(FACTORY_NAME)
+    fun provideFactory(analytics: SelectDocumentAnalytics): ViewModelProvider.Factory =
+        viewModelFactory {
+            initializer {
+                SelectPassportViewModel(analytics)
+            }
+        }
+}
