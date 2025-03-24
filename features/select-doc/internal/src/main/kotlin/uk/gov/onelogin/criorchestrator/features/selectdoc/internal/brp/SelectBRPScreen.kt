12cb502c045cdfb8f89b7a179e671cdb5db939ab
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-03-24 12:13:10 +0000

    chore: add placeholder screens

---
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/brp/SelectBRPScreen.kt
@@ -0,0 +1,25 @@
+package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.brp
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import uk.gov.android.ui.theme.m3.GdsTheme
+import uk.gov.onelogin.criorchestrator.libraries.composeutils.LightDarkBothLocalesPreview
+
+@Composable
+internal fun SelectBRPScreen(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "DCMAW-10690 | Android | Document Selection | Do you have a BRP screen",
+        modifier = modifier,
+    )
+}
+
+@LightDarkBothLocalesPreview
+@Composable
+internal fun PreviewTypesOfPhotoIDScreen() {
+    GdsTheme {
+        SelectBRPScreen()
+    }
+}
---
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/photoid/TypesOfPhotoIDScreen.kt
@@ -0,0 +1,25 @@
+package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.photoid
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import uk.gov.android.ui.theme.m3.GdsTheme
+import uk.gov.onelogin.criorchestrator.libraries.composeutils.LightDarkBothLocalesPreview
+
+@Composable
+internal fun TypesOfPhotoIDScreen(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "DCMAW-8796 | Android | Document Selection | NFC enabled Photo ID Information Screen",
+        modifier = modifier,
+    )
+}
+
+@LightDarkBothLocalesPreview
+@Composable
+internal fun PreviewTypesOfPhotoIDScreen() {
+    GdsTheme {
+        TypesOfPhotoIDScreen()
+    }
+}
