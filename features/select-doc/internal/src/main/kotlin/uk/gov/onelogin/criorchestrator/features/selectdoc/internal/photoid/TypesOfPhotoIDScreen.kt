b57aa1c2cfde6c546b370b2907c098ac3701f10b
Oliver Binns <oliver.binns@digital.cabinet-office.gov.uk>
2025-03-26 11:54:08 +0000

    feat: created Types of Photo ID page

--- features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/photoid/TypesOfPhotoIDScreen.kt
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/photoid/TypesOfPhotoIDScreen.kt
@@ -1,20 +1,201 @@
 package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.photoid
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.collections.immutable.persistentListOf
+import uk.gov.android.ui.componentsv2.bulletedlist.BulletedListTitle
+import uk.gov.android.ui.componentsv2.bulletedlist.GdsBulletedList
+import uk.gov.android.ui.componentsv2.bulletedlist.TitleType.Text
+import uk.gov.android.ui.componentsv2.heading.GdsHeading
+import uk.gov.android.ui.patterns.leftalignedscreen.LeftAlignedScreen
 import uk.gov.android.ui.theme.m3.GdsTheme
-import uk.gov.onelogin.criorchestrator.libraries.composeutils.LightDarkBothLocalesPreview
+import uk.gov.android.ui.theme.util.UnstableDesignSystemAPI
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.R
 
 @Composable
+@OptIn(UnstableDesignSystemAPI::class)
 internal fun TypesOfPhotoIDScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "DCMAW-8796 | Android | Document Selection | NFC enabled Photo ID Information Screen",
+    Surface(
         modifier = modifier,
-    )
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        LeftAlignedScreen(
+            title = { horizontalPadding ->
+                GdsHeading(
+                    text = stringResource(R.string.typesofphotoid_title),
+                    modifier =
+                        Modifier.padding(
+                            horizontal = horizontalPadding,
+                        ),
+                )
+            },
+            body = { horizontalPadding ->
+                item {
+                    Text(
+                        text = stringResource(R.string.typesofphotoid_body),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+                item {
+                    Text(
+                        text = stringResource(R.string.typesofphotoid_ukpassport_title),
+                        fontWeight = FontWeight.W700,
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+                item {
+                    Text(
+                        text = stringResource(R.string.typesofphotoid_ukpassport_body),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    Image(
+                        painter = painterResource(R.drawable.uk_passport),
+                        contentDescription = stringResource(R.string.typesofphotoid_ukpassport_imagedescription),
+                        modifier = Modifier.fillMaxWidth(),
+                        contentScale = ContentScale.FillWidth,
+                    )
+                }
+
+                item {
+                    Text(
+                        text = stringResource(R.string.typesofphotoid_nonukpassport_title),
+                        fontWeight = FontWeight.W700,
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    GdsBulletedList(
+                        title =
+                            BulletedListTitle(
+                                stringResource(R.string.typesofphotoid_nonukpassport_bulletbody),
+                                Text,
+                            ),
+                        bulletListItems =
+                            persistentListOf(
+                                stringResource(R.string.typesofphotoid_nonukpassport_bullet1),
+                                stringResource(R.string.typesofphotoid_nonukpassport_bullet2),
+                            ),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    Text(
+                        text = stringResource(R.string.typesofphotoid_nonukpassport_body),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    Image(
+                        painter = painterResource(R.drawable.nfc_passport_wide),
+                        contentDescription = stringResource(R.string.typesofphotoid_nonukpassport_imagedescription),
+                        modifier = Modifier.fillMaxWidth(),
+                        contentScale = ContentScale.FillWidth,
+                    )
+                }
+
+                item {
+                    Text(
+                        text = stringResource(R.string.typesofphotoid_brp_title),
+                        fontWeight = FontWeight.W700,
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    GdsBulletedList(
+                        title =
+                            BulletedListTitle(
+                                stringResource(R.string.typesofphotoid_brp_bulletbody),
+                                Text,
+                            ),
+                        bulletListItems =
+                            persistentListOf(
+                                stringResource(R.string.typesofphotoid_brp_bullet1),
+                                stringResource(R.string.typesofphotoid_brp_bullet2),
+                                stringResource(R.string.typesofphotoid_brp_bullet3),
+                            ),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    Text(
+                        text = stringResource(R.string.typesofphotoid_brp_body),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    Image(
+                        painter = painterResource(R.drawable.brp),
+                        contentDescription = stringResource(R.string.typesofphotoid_brp_imagedescription),
+                        modifier = Modifier.fillMaxWidth(),
+                        contentScale = ContentScale.FillWidth,
+                    )
+                }
+
+                item {
+                    Text(
+                        text = stringResource(R.string.typesofphotoid_drivinglicence_title),
+                        fontWeight = FontWeight.W700,
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    GdsBulletedList(
+                        title =
+                            BulletedListTitle(
+                                stringResource(R.string.typesofphotoid_drivinglicence_bulletbody),
+                                Text,
+                            ),
+                        bulletListItems =
+                            persistentListOf(
+                                stringResource(R.string.typesofphotoid_drivinglicence_bullet1),
+                                stringResource(R.string.typesofphotoid_drivinglicence_bullet2),
+                            ),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    Text(
+                        text = stringResource(R.string.typesofphotoid_drivinglicence_body),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    Image(
+                        painter = painterResource(R.drawable.brp),
+                        contentDescription = stringResource(R.string.typesofphotoid_drivinglicence_imagedescription),
+                        modifier = Modifier.fillMaxWidth(),
+                        contentScale = ContentScale.FillWidth,
+                    )
+                }
+            },
+        )
+    }
 }
 
-@LightDarkBothLocalesPreview
+@Preview(heightDp = 1600)
 @Composable
 internal fun PreviewTypesOfPhotoIDScreen() {
     GdsTheme {
