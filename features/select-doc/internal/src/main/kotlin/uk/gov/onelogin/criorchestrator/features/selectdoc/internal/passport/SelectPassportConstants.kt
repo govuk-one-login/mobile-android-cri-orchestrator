88e92fe5d8305889b8a0f61f2539af2cfa525e0c
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2025-03-26 17:43:41 +0000

    refactor: Extract constants from 'select passport' screen state (#218)
    https://github.com/govuk-one-login/mobile-android-cri-orchestrator/pull/195#discussion_r2005773517

---
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/passport/SelectPassportConstants.kt
@@ -0,0 +1,12 @@
+package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.passport
+
+import androidx.annotation.StringRes
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.R
+
+internal object SelectPassportConstants {
+    @StringRes val titleId: Int = R.string.selectdocument_passport_title
+
+    @StringRes val readMoreButtonTextId: Int = R.string.selectdocument_passport_readmore_button
+
+    @StringRes val buttonTextId: Int = R.string.selectdocument_passport_continuebutton
+}
--- features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/passport/SelectPassportState.kt
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/passport/SelectPassportState.kt
@@ -1,11 +1,7 @@
 package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.passport
 
-import androidx.annotation.StringRes
 import kotlinx.collections.immutable.PersistentList
 
 data class SelectPassportState(
-    @StringRes val titleId: Int,
-    @StringRes val readMoreButtonTextId: Int,
     val options: PersistentList<Int>,
-    @StringRes val buttonTextId: Int,
 )
