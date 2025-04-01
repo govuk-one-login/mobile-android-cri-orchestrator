5502dbc0fd771f3ca7267d3e13c289a8543835f4
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-04-01 16:55:59 +0100

    fix: lint issues

--- features/id-check-wrapper/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/idcheckwrapper/internal/navigation/MapToDocumentType.kt
+++ features/id-check-wrapper/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/idcheckwrapper/internal/navigation/MapToDocumentType.kt
@@ -4,7 +4,7 @@ import uk.gov.idcheck.repositories.api.webhandover.documenttype.DocumentType
 import uk.gov.onelogin.criorchestrator.features.idcheckwrapper.internalapi.DocumentVariety
 import uk.gov.onelogin.criorchestrator.features.idcheckwrapper.internalapi.IdCheckWrapperDestinations
 
-fun  IdCheckWrapperDestinations.SyncIdCheckScreen.toDocumentType(): DocumentType =
+fun IdCheckWrapperDestinations.SyncIdCheckScreen.toDocumentType(): DocumentType =
     when (documentVariety) {
         DocumentVariety.NFC_PASSPORT -> DocumentType.NFC_PASSPORT
         DocumentVariety.BRP -> DocumentType.BRP
