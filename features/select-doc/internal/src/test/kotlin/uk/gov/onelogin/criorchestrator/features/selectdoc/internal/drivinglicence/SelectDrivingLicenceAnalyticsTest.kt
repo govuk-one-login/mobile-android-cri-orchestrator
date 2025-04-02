2abe96905814d1541f35828385d978b30e09bae7
de1phine <delphine.mubiligi@digital.cabinet-office.gov.uk>
2025-04-02 15:58:35 +0100

    test: add tests for analytics events
    Resolves: DCMAW-8099

---
+++ features/select-doc/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/drivinglicence/SelectDrivingLicenceAnalyticsTest.kt
@@ -0,0 +1,189 @@
+package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.drivinglicence
+
+import android.content.Context
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.performClick
+import androidx.navigation.NavController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.spy
+import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import uk.gov.idcheck.sdk.passport.nfc.checker.NfcChecker
+import uk.gov.logging.api.v3dot1.logger.asLegacyEvent
+import uk.gov.logging.api.v3dot1.model.TrackEvent
+import uk.gov.logging.api.v3dot1.model.ViewEvent
+import uk.gov.onelogin.criorchestrator.features.config.publicapi.Config
+import uk.gov.onelogin.criorchestrator.features.config.publicapi.ConfigStore
+import uk.gov.onelogin.criorchestrator.features.resume.publicapi.nfc.NfcConfigKey
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.R
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.analytics.SelectDocumentAnalytics
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.analytics.SelectDocumentScreenId
+import uk.gov.onelogin.criorchestrator.libraries.analytics.resources.AndroidResourceProvider
+import uk.gov.onelogin.criorchestrator.libraries.testing.MainStandardDispatcherRule
+import uk.gov.onelogin.criorchestrator.libraries.testing.ReportingAnalyticsLoggerRule
+import kotlin.test.assertContains
+
+@RunWith(AndroidJUnit4::class)
+class SelectDrivingLicenceAnalyticsTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @get:Rule
+    val mainStandardDispatcherRule = MainStandardDispatcherRule()
+
+    @get:Rule
+    val reportingAnalyticsLoggerRule = ReportingAnalyticsLoggerRule()
+    private val analyticsLogger = reportingAnalyticsLoggerRule.analyticsLogger
+    private val context: Context = ApplicationProvider.getApplicationContext()
+    private val readMoreButton = hasText(context.getString(R.string.selectdocument_drivinglicence_readmore_button))
+    private val continueButton = hasText(context.getString(R.string.selectdocument_drivinglicence_continuebutton))
+    private val yesOption = hasText(context.getString(R.string.selectdocument_drivinglicence_selection_yes))
+
+    private val configStore: ConfigStore = mock()
+    private val nfcChecker = mock<NfcChecker>()
+    private val navController: NavController = mock()
+
+    private val analytics =
+        SelectDocumentAnalytics(
+            resourceProvider =
+                AndroidResourceProvider(
+                    context = context,
+                ),
+            analyticsLogger = analyticsLogger,
+        )
+
+    private val viewModel: SelectDrivingLicenceViewModel by lazy {
+        spy(
+            SelectDrivingLicenceViewModel(
+                analytics = analytics,
+                nfcChecker = nfcChecker,
+                configStore = configStore,
+            ),
+        )
+    }
+
+    @Before
+    fun setup() {
+        whenever(configStore.readSingle(NfcConfigKey.StubNcfCheck)).thenReturn(
+            Config.Value.BooleanValue(false),
+        )
+    }
+
+    @Test
+    fun `when screen is started and nfc is enabled, it tracks analytics`() {
+        given(nfcChecker.hasNfc()).willReturn(true)
+
+        val expectedEvent =
+            ViewEvent
+                .Screen(
+                    id = SelectDocumentScreenId.SelectDrivingLicence.rawId,
+                    name = context.getString(R.string.selectdocument_drivinglicence_title),
+                    params = SelectDocumentAnalytics.requiredParameters,
+                ).asLegacyEvent()
+
+        composeTestRule.setSelectDrivingLicenceContent()
+
+        assertContains(analyticsLogger.loggedEvents, expectedEvent)
+    }
+
+    @Test
+    fun `when screen is started and nfc is not enabled, it tracks analytics`() {
+        given(nfcChecker.hasNfc()).willReturn(false)
+
+        val expectedEvent =
+            ViewEvent
+                .Screen(
+                    id = SelectDocumentScreenId.SelectDrivingLicence.rawId,
+                    name = context.getString(R.string.selectdocument_drivinglicence_title),
+                    params = SelectDocumentAnalytics.requiredParameters,
+                ).asLegacyEvent()
+
+        composeTestRule.setSelectDrivingLicenceContent()
+
+        assertContains(analyticsLogger.loggedEvents, expectedEvent)
+    }
+
+    @Test
+    fun `when nfc is enabled and continue button is clicked, it tracks analytics`() {
+        given(nfcChecker.hasNfc()).willReturn(true)
+
+        composeTestRule.setSelectDrivingLicenceContent()
+
+        composeTestRule
+            .onNode(yesOption, useUnmergedTree = true)
+            .performClick()
+
+        composeTestRule
+            .onNode(continueButton)
+            .performClick()
+
+        val expectedEvent =
+            TrackEvent
+                .Form(
+                    text = context.getString(R.string.selectdocument_passport_continuebutton),
+                    params = SelectDocumentAnalytics.requiredParameters,
+                    response = context.getString(R.string.selectdocument_passport_selection_yes),
+                ).asLegacyEvent()
+        assertContains(analyticsLogger.loggedEvents, expectedEvent)
+    }
+
+    @Test
+    fun `when nfc is not enabled and continue button is clicked, it tracks analytics`() {
+        given(nfcChecker.hasNfc()).willReturn(true)
+
+        composeTestRule.setSelectDrivingLicenceContent()
+
+        composeTestRule
+            .onNode(yesOption, useUnmergedTree = true)
+            .performClick()
+
+        composeTestRule
+            .onNode(continueButton)
+            .performClick()
+
+        val expectedEvent =
+            TrackEvent
+                .Form(
+                    text = context.getString(R.string.selectdocument_drivinglicence_continuebutton),
+                    params = SelectDocumentAnalytics.requiredParameters,
+                    response = context.getString(R.string.selectdocument_drivinglicence_selection_yes),
+                ).asLegacyEvent()
+        assertContains(analyticsLogger.loggedEvents, expectedEvent)
+    }
+
+    @Test
+    fun `when nfc is enabled and read more is clicked, it tracks analytics`() {
+        given(nfcChecker.hasNfc()).willReturn(true)
+
+        composeTestRule.setSelectDrivingLicenceContent()
+
+        composeTestRule
+            .onNode(readMoreButton)
+            .performClick()
+
+        val expectedEvent =
+            TrackEvent
+                .Button(
+                    text = context.getString(R.string.selectdocument_drivinglicence_readmore_button),
+                    params = SelectDocumentAnalytics.requiredParameters,
+                ).asLegacyEvent()
+        assertContains(analyticsLogger.loggedEvents, expectedEvent)
+    }
+
+    private fun ComposeContentTestRule.setSelectDrivingLicenceContent() {
+        setContent {
+            SelectDrivingLicenceScreen(
+                viewModel = viewModel,
+                navController = navController,
+            )
+        }
+    }
+}
