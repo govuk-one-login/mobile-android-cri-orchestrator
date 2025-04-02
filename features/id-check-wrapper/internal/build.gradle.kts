15a1422d0ad0e77f1d53d9d129a497481122506c
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-04-02 10:55:52 +0100

    chore: tidy up build gradle files

--- features/id-check-wrapper/internal/build.gradle.kts
+++ features/id-check-wrapper/internal/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.uk.gov.idcheck.repositories.api)
-    implementation(libs.uk.gov.idcheck.ui.presentation)
     implementation(projects.features.idCheckWrapper.internalApi)
     implementation(projects.features.session.internalApi)
     implementation(project(":features:resume:internal-api"))
