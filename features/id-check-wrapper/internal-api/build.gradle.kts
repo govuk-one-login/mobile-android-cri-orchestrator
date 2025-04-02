62146dc26718777e24079498ce1d89f03b2dabf3
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-04-02 10:55:52 +0100

    chore: tidy up build gradle files

--- features/id-check-wrapper/internal-api/build.gradle.kts
+++ features/id-check-wrapper/internal-api/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
     id("uk.gov.onelogin.criorchestrator.android-lib-config")
-    id("uk.gov.onelogin.criorchestrator.ui-config")
     alias(libs.plugins.kotlin.serialization)
 }
 
 dependencies {
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.uk.gov.idcheck.repositories.api)
-    implementation(libs.uk.gov.idcheck.ui.presentation)
     implementation(project(":features:resume:internal-api"))
     implementation(project(":libraries:di"))
     implementation(project(":libraries:navigation"))
