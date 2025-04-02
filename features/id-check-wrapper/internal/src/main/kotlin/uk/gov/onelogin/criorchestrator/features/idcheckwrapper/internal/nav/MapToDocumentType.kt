761908163e8daf20847d716dd14c08f48c4e5bad
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-04-02 11:31:43 +0100

    refactor: navigate using DocumentVariety instead of DocumentType

--- features/id-check-wrapper/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/idcheckwrapper/internal/nav/MapToDocumentType.kt
+++ features/id-check-wrapper/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/idcheckwrapper/internal/nav/MapToDocumentType.kt
@@ -2,10 +2,9 @@ package uk.gov.onelogin.criorchestrator.features.idcheckwrapper.internal.nav
 
 import uk.gov.idcheck.repositories.api.webhandover.documenttype.DocumentType
 import uk.gov.onelogin.criorchestrator.features.idcheckwrapper.internalapi.DocumentVariety
-import uk.gov.onelogin.criorchestrator.features.idcheckwrapper.internalapi.nav.IdCheckWrapperDestinations
 
-fun IdCheckWrapperDestinations.SyncIdCheckScreen.toDocumentType(): DocumentType =
-    when (documentVariety) {
+fun DocumentVariety.toDocumentType(): DocumentType =
+    when (this) {
         DocumentVariety.NFC_PASSPORT -> DocumentType.NFC_PASSPORT
         DocumentVariety.BRP -> DocumentType.BRP
         DocumentVariety.DRIVING_LICENCE -> DocumentType.DRIVING_LICENCE
