186e7a370aa89047fdd066f11a25bff36c3f45ff
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-04-03 11:21:44 +0100

    refactor: Fix deprecated `FullScreenDialog`

--- test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenuDialog.kt
+++ test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenuDialog.kt
@@ -3,7 +3,7 @@ package uk.gov.onelogin.criorchestrator.testwrapper.devmenu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import uk.gov.android.ui.patterns.dialog.FullScreenDialog
+import uk.gov.android.ui.patterns.dialog.FullScreenDialogue
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.onelogin.criorchestrator.features.dev.publicapi.DevMenuScreen
 import uk.gov.onelogin.criorchestrator.sdk.sharedapi.CriOrchestratorComponent
@@ -16,7 +16,7 @@ internal fun DevMenuDialog(
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    FullScreenDialog(
+    FullScreenDialogue(
         modifier = modifier,
         onDismissRequest = onDismissRequest,
     ) {
