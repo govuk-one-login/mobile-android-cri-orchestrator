2d61e5ea238197875b433f4f7295ca1dd4944f70
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-04-04 09:20:12 +0100

    fix: persist selected option in select passport screen

--- features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/passport/SelectPassportScreen.kt
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/passport/SelectPassportScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -78,7 +79,7 @@ internal fun SelectPassportScreenContent(
     onConfirmSelection: (Int) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    var selectedItem by remember { mutableStateOf<Int?>(null) }
+    var selectedItem by rememberSaveable { mutableStateOf<Int?>(null) }
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background,
