43334ac277a4288002ae382c61dd3527e3a643c6
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-04-09 12:01:22 +0100

    fix: remove unnecessary error screen id

--- features/error/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/error/internal/analytics/ErrorScreenId.kt
+++ features/error/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/error/internal/analytics/ErrorScreenId.kt
@@ -5,6 +5,5 @@ import uk.gov.onelogin.criorchestrator.libraries.analytics.ScreenId
 enum class ErrorScreenId(
     override val rawId: String,
 ) : ScreenId {
-    UnrecoverableError(rawId = "80069598-8c97-4789-96f8-8930fb633889"),
     RecoverableError(rawId = "80606f36-f6aa-4f49-aaa8-ff7d3cdeb16f"),
 }
