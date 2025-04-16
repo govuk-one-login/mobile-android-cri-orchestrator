bdd6328cd1524cbea8d53b656a87eaa752b7b8a7
de1phine <128810121+de1phine@users.noreply.github.com>
2025-04-16 09:10:54 +0100

    feat: Create No NFC Abort Confirmation screen (#307)

---
+++ features/select-doc/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/confirmnoid/nononchippedid/ConfirmNoNonChippedIDScreen.kt
@@ -0,0 +1,112 @@
+package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.confirmnoid.nononchippedid
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import kotlinx.collections.immutable.persistentListOf
+import uk.gov.android.ui.componentsv2.bulletedlist.BulletedListTitle
+import uk.gov.android.ui.componentsv2.bulletedlist.GdsBulletedList
+import uk.gov.android.ui.componentsv2.bulletedlist.TitleType
+import uk.gov.android.ui.componentsv2.button.ButtonType
+import uk.gov.android.ui.componentsv2.button.GdsButton
+import uk.gov.android.ui.componentsv2.heading.GdsHeading
+import uk.gov.android.ui.componentsv2.heading.GdsHeadingAlignment
+import uk.gov.android.ui.patterns.leftalignedscreen.LeftAlignedScreen
+import uk.gov.android.ui.theme.m3.GdsTheme
+import uk.gov.android.ui.theme.util.UnstableDesignSystemAPI
+import uk.gov.onelogin.criorchestrator.features.handback.internalapi.nav.HandbackDestinations
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.R
+import uk.gov.onelogin.criorchestrator.libraries.composeutils.LightDarkBothLocalesPreview
+
+@Composable
+internal fun ConfirmNoNonChippedIDScreen(
+    viewModel: ConfirmNoNonChippedIDViewModel,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+) {
+    LaunchedEffect(Unit) {
+        viewModel.onScreenStart()
+
+        viewModel.action.collect { event ->
+            when (event) {
+                ConfirmNoNonChippedIDAction.NavigateToConfirmAbort -> {
+                    navController.navigate(HandbackDestinations.ConfirmAbort)
+                }
+            }
+        }
+    }
+
+    ConfirmNoNonChippedIDContent(
+        onConfirmClick = viewModel::onConfirmClick,
+        modifier = modifier,
+    )
+}
+
+@OptIn(UnstableDesignSystemAPI::class)
+@Composable
+internal fun ConfirmNoNonChippedIDContent(
+    onConfirmClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        LeftAlignedScreen(
+            title = { horizontalPadding ->
+                GdsHeading(
+                    stringResource(ConfirmNoNonChippedIDConstants.titleId),
+                    textAlign = GdsHeadingAlignment.LeftAligned,
+                    modifier = Modifier.padding(horizontal = horizontalPadding),
+                )
+            },
+            body = { horizontalPadding ->
+                item {
+                    Text(
+                        text = stringResource(R.string.confirm_nononchippedid_body_1),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+                item {
+                    GdsBulletedList(
+                        persistentListOf(
+                            stringResource(R.string.confirm_nononchippedid_bullet_1),
+                            stringResource(R.string.confirm_nononchippedid_bullet_2),
+                        ),
+                        title =
+                            BulletedListTitle(
+                                text = stringResource(R.string.confirm_nononchippedid_body_2),
+                                titleType = TitleType.Text,
+                            ),
+                        modifier = Modifier.padding(horizontal = horizontalPadding),
+                    )
+                }
+            },
+            primaryButton = {
+                GdsButton(
+                    stringResource(ConfirmNoNonChippedIDConstants.confirmButtonTextId),
+                    buttonType = ButtonType.Primary,
+                    onClick = onConfirmClick,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            },
+        )
+    }
+}
+
+@LightDarkBothLocalesPreview
+@Composable
+internal fun PreviewConfirmNoNonChippedIDScreen() {
+    GdsTheme {
+        ConfirmNoNonChippedIDContent(
+            onConfirmClick = {},
+        )
+    }
+}
---
+++ features/select-doc/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/selectdoc/internal/confirmnoid/nononchippedid/ConfirmNoNonChippedIDScreenAnalyticsTest.kt
@@ -0,0 +1,93 @@
+package uk.gov.onelogin.criorchestrator.features.selectdoc.internal.confirmnoid.nononchippedid
+
+import android.content.Context
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.rememberNavController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import uk.gov.logging.api.v3dot1.logger.asLegacyEvent
+import uk.gov.logging.api.v3dot1.model.TrackEvent
+import uk.gov.logging.api.v3dot1.model.ViewEvent
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.R
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.analytics.SelectDocAnalytics
+import uk.gov.onelogin.criorchestrator.features.selectdoc.internal.analytics.SelectDocScreenId
+import uk.gov.onelogin.criorchestrator.libraries.analytics.resources.AndroidResourceProvider
+import uk.gov.onelogin.criorchestrator.libraries.testing.MainStandardDispatcherRule
+import uk.gov.onelogin.criorchestrator.libraries.testing.ReportingAnalyticsLoggerRule
+import kotlin.test.assertContains
+
+@RunWith(AndroidJUnit4::class)
+class ConfirmNoNonChippedIDScreenAnalyticsTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @get:Rule
+    val mainStandardDispatcherRule = MainStandardDispatcherRule()
+
+    @get:Rule
+    val reportingAnalyticsLoggerRule = ReportingAnalyticsLoggerRule()
+    private val analyticsLogger = reportingAnalyticsLoggerRule.analyticsLogger
+    private val context: Context = ApplicationProvider.getApplicationContext()
+
+    private val confirmButton =
+        hasText(context.getString(R.string.confirm_nononchippedid_confirmbutton))
+
+    private val analytics =
+        SelectDocAnalytics(
+            resourceProvider =
+                AndroidResourceProvider(
+                    context = context,
+                ),
+            analyticsLogger = analyticsLogger,
+        )
+
+    private val viewModel =
+        ConfirmNoNonChippedIDViewModel(
+            analytics = analytics,
+        )
+
+    @Test
+    fun `when screen is started, it tracks analytics`() {
+        val expectedEvent =
+            ViewEvent
+                .Screen(
+                    id = SelectDocScreenId.ConfirmNoNonChippedID.rawId,
+                    name = context.getString(R.string.confirm_nononchippedid_title),
+                    params = SelectDocAnalytics.requiredParameters,
+                ).asLegacyEvent()
+        composeTestRule.setConfirmNoNonChippedIDScreenContent()
+        assertContains(analyticsLogger.loggedEvents, expectedEvent)
+    }
+
+    @Test
+    fun `when confirm button is clicked, it tracks analytics`() {
+        composeTestRule.setConfirmNoNonChippedIDScreenContent()
+
+        composeTestRule
+            .onNode(confirmButton)
+            .performClick()
+
+        val expectedEvent =
+            TrackEvent
+                .Button(
+                    text = context.getString(R.string.confirm_nononchippedid_confirmbutton),
+                    params = SelectDocAnalytics.requiredParameters,
+                ).asLegacyEvent()
+        assertContains(analyticsLogger.loggedEvents, expectedEvent)
+    }
+
+    private fun ComposeContentTestRule.setConfirmNoNonChippedIDScreenContent() {
+        setContent {
+            ConfirmNoNonChippedIDScreen(
+                viewModel = viewModel,
+                navController = rememberNavController(),
+            )
+        }
+    }
+}
