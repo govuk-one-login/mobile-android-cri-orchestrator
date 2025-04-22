fda5b12288012816112de147167979e215df419f
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-04-22 14:12:58 +0100

    chore: reword text in test screen

--- features/id-check-wrapper/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/idcheckwrapper/internal/screen/SyncIdCheckManualBiometricTokenContent.kt
+++ features/id-check-wrapper/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/idcheckwrapper/internal/screen/SyncIdCheckManualBiometricTokenContent.kt
@@ -24,7 +24,7 @@ internal fun SyncIdCheckManualBiometricTokenContent(
         GdsSelection(
             title =
                 RadioSelectionTitle(
-                    "Select get biometric token result",
+                    "Select biometric token result",
                     titleType = TitleType.BoldText,
                 ),
             items =
