61521fcc975350458ad3ac4a6496f55c31517f7c
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-05-06 16:50:57 +0100

    fix: Fix crash on launching ID Check SDK

--- build-logic/plugins/src/main/kotlin/uk/gov/onelogin/criorchestrator/android-lib-config.gradle.kts
+++ build-logic/plugins/src/main/kotlin/uk/gov/onelogin/criorchestrator/android-lib-config.gradle.kts
@@ -4,6 +4,7 @@ import com.android.build.api.dsl.LibraryExtension
 import org.gradle.accessors.dm.LibrariesForLibs
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import uk.gov.onelogin.criorchestrator.extensions.androidTestDependencies
+import uk.gov.onelogin.criorchestrator.extensions.excludeAndroidClassesFromJacocoCoverage
 import uk.gov.onelogin.criorchestrator.extensions.disableJavadocGeneration
 import uk.gov.onelogin.criorchestrator.extensions.setInstrumentationTestingConfig
 import uk.gov.onelogin.criorchestrator.extensions.setJavaVersion
@@ -37,6 +38,9 @@ configure<KotlinAndroidProjectExtension> {
 // https://govukverify.atlassian.net/browse/DCMAW-11888
 // https://github.com/Kotlin/dokka/issues/2956
 project.disableJavadocGeneration()
+
+// https://github.com/cashapp/paparazzi/issues/955
+project.excludeAndroidClassesFromJacocoCoverage()
 
 dependencies {
     androidTestDependencies(libs)
