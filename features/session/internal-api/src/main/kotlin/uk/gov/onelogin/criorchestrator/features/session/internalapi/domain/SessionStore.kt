uk-gov-mirror/govuk-one-login.mobile-android-cri-orchestrator

feat: Hide 'prove your identity card' in some scenarios

features/session/internal-api/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internalapi/domain/SessionStore.kt
@@ -5,5 +5,11 @@ import kotlinx.coroutines.flow.StateFlow
 interface SessionStore {
     fun read(): StateFlow<Session?>
 
-    fun write(value: Session?)
+    fun write(value: Session)
+
+    fun clear()
+
+    fun updateToNotResumable()
+
+    fun updateToAborted()
 }

govuk-one-login/mobile-android-cri-orchestrator

features/session/internal-api/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internalapi/domain/SessionStore.kt
@@ -5,5 +5,11 @@ import kotlinx.coroutines.flow.StateFlow
 interface SessionStore {
     fun read(): StateFlow<Session?>
 
-    fun write(value: Session?)
+    fun write(value: Session)
+
+    fun clear()
+
+    fun updateToDocumentSelected()
+
+    fun updateToAborted()
 }