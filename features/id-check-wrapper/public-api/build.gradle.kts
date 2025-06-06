a2dff9dcbf8c1f8308d4f954c976bffa0f918f8a
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2025-06-06 09:21:03 +0100

    feat: Add version constraint for ID Check SDK hilt config dependency (#380)

--- features/id-check-wrapper/public-api/build.gradle.kts
+++ features/id-check-wrapper/public-api/build.gradle.kts
@@ -3,6 +3,19 @@ plugins {
 }
 
 dependencies {
+    // If the consuming app has a dependency on the ID Check SDK hilt config, prefer that the
+    // version is compatible with the version of the SDK used in this project.
+    constraints {
+        // This is just a constraint, not a dependency.
+        api("uk.gov.onelogin.idcheck:hilt-config") {
+            version {
+                prefer(
+                    libs.versions.gov.uk.idcheck
+                        .get(),
+                )
+            }
+        }
+    }
     implementation(projects.features.config.publicApi)
 }
 
