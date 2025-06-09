3a9df01cf9e66d5ceb2b637c4ce5bea1b69ec7eb
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-06-09 10:37:24 +0100

    docs: Add kdoc for session interfaces

--- features/session/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/network/activesession/ActiveSessionApi.kt
+++ features/session/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internal/network/activesession/ActiveSessionApi.kt
@@ -2,6 +2,20 @@ package uk.gov.onelogin.criorchestrator.features.session.internal.network.active
 
 import uk.gov.android.network.api.ApiResponse
 
+/**
+ * The ID Check async backend's 'active session' API.
+ *
+ * https://github.com/govuk-one-login/mobile-id-check-async
+ */
 fun interface ActiveSessionApi {
+    /**
+     * Get the 'active' ID Check session if it exists and hasn't expired.
+     *
+     * Retrieves the session from the ID Check async API.
+     *
+     * A session is considered active if:
+     * - the user hasn't selected a document, and
+     * - the user hasn't aborted the session
+     */
     suspend fun getActiveSession(): ApiResponse
 }
