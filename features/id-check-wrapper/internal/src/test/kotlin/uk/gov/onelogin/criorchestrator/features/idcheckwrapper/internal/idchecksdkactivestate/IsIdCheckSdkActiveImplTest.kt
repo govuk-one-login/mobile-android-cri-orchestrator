d0c08ad4193502821a6b36c58c79980dee221f1c
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-06-24 10:19:23 +0100

    test: add tests for IdCheckSdkActiveState

---
+++ features/id-check-wrapper/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/idcheckwrapper/internal/idchecksdkactivestate/IsIdCheckSdkActiveImplTest.kt
@@ -0,0 +1,24 @@
+package uk.gov.onelogin.criorchestrator.features.idcheckwrapper.internal.idchecksdkactivestate
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import uk.gov.logging.testdouble.SystemLogger
+
+class IsIdCheckSdkActiveImplTest {
+    private val logger = SystemLogger()
+    private val idCheckSdkActiveStateStore = InMemoryIdCheckSdkActiveStateStore(logger)
+    private val isIdCheckSdkActiveImpl = IsIdCheckSdkActiveImpl(idCheckSdkActiveStateStore)
+
+    @Test
+    fun `given ID Check SDK is inactive, then isIdCheckSdkActiveImpl returns false`() {
+        idCheckSdkActiveStateStore.setInactive()
+        assertFalse(isIdCheckSdkActiveImpl())
+    }
+
+    @Test
+    fun `given ID Check SDK is active, then isIdCheckSdkActiveImpl returns true`() {
+        idCheckSdkActiveStateStore.setActive()
+        assertTrue(isIdCheckSdkActiveImpl())
+    }
+}
