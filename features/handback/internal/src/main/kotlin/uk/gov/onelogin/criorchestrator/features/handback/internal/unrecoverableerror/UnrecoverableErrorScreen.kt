5916aae12d2192a6073fe3417cf3a0b5a9236e52
Emanuel Canha <emanuel.canha@digital.cabinet-office.gov.uk>
2025-06-25 09:13:43 +0100

    chore: migrate to new error screen pattern

--- features/handback/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/handback/internal/unrecoverableerror/UnrecoverableErrorScreen.kt
+++ features/handback/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/handback/internal/unrecoverableerror/UnrecoverableErrorScreen.kt
@@ -2,19 +2,26 @@
 
 package uk.gov.onelogin.criorchestrator.features.handback.internal.unrecoverableerror
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.compose.dropUnlessResumed
 import androidx.navigation.NavController
-import kotlinx.collections.immutable.persistentListOf
-import uk.gov.android.ui.patterns.centrealignedscreen.CentreAlignedScreenBodyContent
-import uk.gov.android.ui.patterns.centrealignedscreen.CentreAlignedScreenButton
-import uk.gov.android.ui.patterns.errorscreen.ErrorScreen
-import uk.gov.android.ui.patterns.errorscreen.ErrorScreenIcon
+import uk.gov.android.ui.componentsv2.button.ButtonType
+import uk.gov.android.ui.componentsv2.button.GdsButton
+import uk.gov.android.ui.componentsv2.heading.GdsHeading
+import uk.gov.android.ui.componentsv2.images.GdsIcon
+import uk.gov.android.ui.patterns.errorscreen.v2.ErrorScreen
+import uk.gov.android.ui.patterns.errorscreen.v2.ErrorScreenIcon
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.android.ui.theme.util.UnstableDesignSystemAPI
 import uk.gov.onelogin.criorchestrator.features.handback.internal.R
@@ -58,6 +65,7 @@ internal fun UnrecoverableErrorScreen(
     )
 }
 
+@Suppress("LongMethod")
 @OptIn(UnstableDesignSystemAPI::class)
 @Composable
 internal fun UnrecoverableErrorScreenContent(
@@ -69,22 +77,60 @@ internal fun UnrecoverableErrorScreenContent(
         color = MaterialTheme.colorScheme.background,
     ) {
         ErrorScreen(
-            title = stringResource(UnrecoverableErrorConstants.titleId),
-            body =
-                persistentListOf(
-                    CentreAlignedScreenBodyContent.Text(
-                        bodyText = stringResource(R.string.handback_unrecoverableerror_body1),
-                    ),
-                    CentreAlignedScreenBodyContent.Text(
-                        bodyText = stringResource(R.string.handback_unrecoverableerror_body2),
-                    ),
-                ),
-            icon = ErrorScreenIcon.ErrorIcon,
-            secondaryButton =
-                CentreAlignedScreenButton(
+            icon = { horizontalPadding ->
+                GdsIcon(
+                    image = ImageVector.vectorResource(ErrorScreenIcon.ErrorIcon.icon),
+                    contentDescription = stringResource(ErrorScreenIcon.ErrorIcon.description),
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = horizontalPadding),
+                )
+            },
+            title = { horizontalPadding ->
+                GdsHeading(
+                    stringResource(UnrecoverableErrorConstants.titleId),
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = horizontalPadding),
+                )
+            },
+            body = { horizontalPadding ->
+                item {
+                    Text(
+                        text = stringResource(R.string.handback_unrecoverableerror_body1),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center,
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                item {
+                    Text(
+                        text = stringResource(R.string.handback_unrecoverableerror_body2),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center,
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = horizontalPadding),
+                    )
+                }
+            },
+            secondaryButton = {
+                GdsButton(
                     text = stringResource(UnrecoverableErrorConstants.buttonTextId),
+                    buttonType = ButtonType.Secondary,
                     onClick = dropUnlessResumed { onButtonClick() },
-                ),
+                    modifier =
+                        Modifier
+                            .fillMaxWidth(),
+                )
+            },
         )
     }
 }
