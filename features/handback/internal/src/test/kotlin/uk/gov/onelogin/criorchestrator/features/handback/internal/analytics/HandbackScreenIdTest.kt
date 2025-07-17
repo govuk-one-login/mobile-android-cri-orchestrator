871da576bbe7c84905d035386d40c1c58814fc04
JacksonJ2W <152998610+JacksonJ2W@users.noreply.github.com>
2025-07-17 09:03:31 +0100

    fix: correct analytics for ConfirmAbortDesktop screen (#428)

--- features/handback/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/handback/internal/analytics/HandbackScreenIdTest.kt
+++ features/handback/internal/src/test/kotlin/uk/gov/onelogin/criorchestrator/features/handback/internal/analytics/HandbackScreenIdTest.kt
@@ -9,7 +9,7 @@ class HandbackScreenIdTest {
         assertEquals("80069598-8c97-4789-96f8-8930fb633889", HandbackScreenId.UnrecoverableError.rawId)
         assertEquals("fff9c5fe-3eb2-45d4-a07a-fb6d21582c50", HandbackScreenId.ReturnToDesktopWeb.rawId)
         assertEquals("4a9fafaa-5359-4105-b223-a51d71df435f", HandbackScreenId.ReturnToMobileWeb.rawId)
-        assertEquals("8d09e79f-f242-48c8-a456-4f302c23e6cb", HandbackScreenId.ConfirmAbortDesktop.rawId)
+        assertEquals("5aa9f4a5-3312-42e4-b80a-432e237256a7", HandbackScreenId.ConfirmAbortDesktop.rawId)
         assertEquals("8d09e79f-f242-48c8-a456-4f302c23e6cb", HandbackScreenId.AbortedReturnToDesktopWeb.rawId)
         assertEquals("c4b6167a-09b3-4671-b9d7-30f7acd02bdb", HandbackScreenId.ConfirmAbortMobile.rawId)
     }
