a9260a149f3fc0d2fc9c8e36f62a8f33525220dd
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2025-07-24 22:45:18 +0100

    docs: fix typo in kdoc

--- features/id-check-wrapper/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/idcheckwrapper/internal/activity/UnavailableIdCheckSdkActivityResultContract.kt
+++ features/id-check-wrapper/internal/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/idcheckwrapper/internal/activity/UnavailableIdCheckSdkActivityResultContract.kt
@@ -10,7 +10,7 @@ import uk.gov.idcheck.sdk.IdCheckSdkParameters
 /**
  * This contract is unable to launch intents, but is able to parse intents and transform them into exit states.
  * This is because if the app activity that launches the ID Check SDK dies during the ID Check SDK journey, when
- * the ID Check SDK hands back to the CRI Orchestrator screen, the activity is recreated and thos contract is used
+ * the ID Check SDK hands back to the CRI Orchestrator screen, the activity is recreated and this contract is used
  * by default and so must be able to parse the intent correctly.
  */
 internal class UnavailableIdCheckSdkActivityResultContract :
