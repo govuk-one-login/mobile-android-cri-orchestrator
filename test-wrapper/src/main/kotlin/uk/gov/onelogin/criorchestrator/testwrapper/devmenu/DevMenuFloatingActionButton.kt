535bd49dba10476ce7b55f2a00ad4a64c9d62d02
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2025-10-01 10:32:51 +0100

    refactor: Migrate to XML icons

--- test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenuFloatingActionButton.kt
+++ test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenuFloatingActionButton.kt
@@ -1,13 +1,14 @@
 package uk.gov.onelogin.criorchestrator.testwrapper.devmenu
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import uk.gov.onelogin.criorchestrator.testwrapper.R
 
 @Composable
 internal fun DevMenuFloatingActionButton(
@@ -17,8 +18,9 @@ internal fun DevMenuFloatingActionButton(
     ExtendedFloatingActionButton(
         modifier = modifier,
         onClick = onClick,
-        icon = { Icon(Icons.Filled.Settings, null) },
+        icon = { Icon(painterResource(R.drawable.ic_settings), null) },
         text = { Text(text = "Developer settings") },
-        containerColor = MaterialTheme.colorScheme.outlineVariant,
+        containerColor = MaterialTheme.colorScheme.secondary,
+        shape = FloatingActionButtonDefaults.smallShape,
     )
 }
