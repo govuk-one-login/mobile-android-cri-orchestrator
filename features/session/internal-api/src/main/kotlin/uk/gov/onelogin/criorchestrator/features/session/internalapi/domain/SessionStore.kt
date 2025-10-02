ab0c64e726137f4295933abd4c4e9a3f0f952e2b
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2025-10-02 16:50:44 +0100

    docs: Add kdoc for session interfaces (#383)

--- features/session/internal-api/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internalapi/domain/SessionStore.kt
+++ features/session/internal-api/src/main/kotlin/uk/gov/onelogin/criorchestrator/features/session/internalapi/domain/SessionStore.kt
@@ -2,14 +2,42 @@ package uk.gov.onelogin.criorchestrator.features.session.internalapi.domain
 
 import kotlinx.coroutines.flow.StateFlow
 
+/**
+ * Stores and retrieves the known state of the user's ID Check session (see [Session]).
+ *
+ * Implementations may or may not persist the data.
+ */
 interface SessionStore {
+    /**
+     * Get the last known state of the user's ID Check session.
+     */
     fun read(): StateFlow<Session?>
 
+    /**
+     * Store the user's ID Check session.
+     */
     fun write(value: Session)
 
+    /**
+     * Clear the session.
+     *
+     * After calling, [read] emits `null`.
+     */
     fun clear()
 
+    /**
+     * Update the stored [Session.sessionState] to at least [Session.State.DocumentSelected].
+     *
+     * If there is no [Session] or [Session.sessionState] has already advanced to or beyond
+     * this state, this function does nothing.
+     */
     fun updateToDocumentSelected()
 
+    /**
+     * Update the stored [Session.sessionState] to at least [Session.State.Aborted].
+     *
+     * If there is no [Session] or [Session.sessionState] has already advanced to or beyond
+     * this state, this function does nothing.
+     */
     fun updateToAborted()
 }
