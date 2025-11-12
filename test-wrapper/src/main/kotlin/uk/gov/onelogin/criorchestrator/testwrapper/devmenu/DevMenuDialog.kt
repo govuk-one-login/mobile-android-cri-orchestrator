7d73e15bff6f55512780642ae95dcc7da5e04f46
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2025-11-12 09:15:07 +0000

    fix: Fix test wrapper dev menu dialog behaviour (#569)
    Since govuk-one-login/mobile-android-ui#304, FullScreenDialog isn't a dialog. It must be wrapped in a Dialog component by the consumer.

--- test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenuDialog.kt
+++ test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/devmenu/DevMenuDialog.kt
@@ -3,9 +3,11 @@ package uk.gov.onelogin.criorchestrator.testwrapper.devmenu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.window.Dialog
 import uk.gov.android.ui.patterns.dialog.FullScreenDialogue
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.onelogin.criorchestrator.features.dev.publicapi.DevMenuScreen
+import uk.gov.onelogin.criorchestrator.libraries.composeutils.fullScreenDialogProperties
 import uk.gov.onelogin.criorchestrator.sdk.sharedapi.CriOrchestratorGraph
 import uk.gov.onelogin.criorchestrator.testwrapper.preview.rememberPreviewCriOrchestratorGraph
 
@@ -15,6 +17,9 @@ internal fun DevMenuDialog(
     criOrchestratorGraph: CriOrchestratorGraph,
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
+) = Dialog(
+    onDismissRequest = onDismissRequest,
+    properties = fullScreenDialogProperties,
 ) {
     FullScreenDialogue(
         modifier = modifier,
