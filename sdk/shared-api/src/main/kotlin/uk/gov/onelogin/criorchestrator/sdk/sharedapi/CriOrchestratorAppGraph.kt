1283f9262f27ecc7f6b5b9f1c924c0517542745e
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2026-01-27 14:15:29 +0000

    refactor!: Remove deprecated 'component' APIs (#676)
    - Remove CriOrchestratorComponent interface (replaced by CriOrchestratorGraph)
    - Remove CriOrchestratorSingletonComponent interface (replaced by CriOrchestratorAppGraph)
    - Remove deprecated DevMenuScreen overload accepting component parameter
    - Remove deprecated ProveYourIdentityCard overload accepting component parameter
    
    DCMAW-16255
    
    - Removes code deprecated in https://github.com/govuk-one-login/mobile-android-cri-orchestrator/pull/483

--- sdk/shared-api/src/main/kotlin/uk/gov/onelogin/criorchestrator/sdk/sharedapi/CriOrchestratorAppGraph.kt
+++ sdk/shared-api/src/main/kotlin/uk/gov/onelogin/criorchestrator/sdk/sharedapi/CriOrchestratorAppGraph.kt
@@ -7,15 +7,5 @@ import uk.gov.onelogin.criorchestrator.libraries.di.CriOrchestratorAppScope
  * Public-facing abstraction that hides the internal dependency graph
  * (`BaseCriOrchestratorSdkGraph`) and provides no functionality.
  */
-@Deprecated(
-    "Renamed to CriOrchestratorAppGraph. To be removed 15/11/2025.",
-    replaceWith = ReplaceWith("CriOrchestratorAppGraph"),
-)
-interface CriOrchestratorSingletonComponent
-
-/**
- * Public-facing abstraction that hides the internal dependency graph
- * (`BaseCriOrchestratorSdkGraph`) and provides no functionality.
- */
 @ContributesTo(CriOrchestratorAppScope::class)
-interface CriOrchestratorAppGraph : CriOrchestratorSingletonComponent
+interface CriOrchestratorAppGraph
