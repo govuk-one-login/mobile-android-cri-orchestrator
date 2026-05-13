90db990b2389564c38d4e4ad6e49e4c79bd44356
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2026-05-13 14:46:06 +0100

    feat: Fix DI for networking library

--- test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/hilt/CriOrchestratorSdkHiltModule.kt
+++ test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/hilt/CriOrchestratorSdkHiltModule.kt
@@ -23,7 +23,7 @@ object CriOrchestratorSdkHiltModule {
         analyticsLogger: AnalyticsLogger,
         application: Application,
         logger: Logger,
-        httpClient: GenericHttpClient,
+        @HttpClientGeneric httpClient: GenericHttpClient,
         resources: Resources,
     ): CriOrchestratorSdk =
         CriOrchestratorSdk.create(
---
+++ test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/hilt/HttpClientHiltModule.kt
@@ -0,0 +1,27 @@
+package uk.gov.onelogin.criorchestrator.testwrapper.hilt
+
+import android.content.res.Resources
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import uk.gov.android.network.client.GenericHttpClient
+import uk.gov.onelogin.criorchestrator.testwrapper.SubjectTokenRepository
+import uk.gov.onelogin.criorchestrator.testwrapper.network.createHttpClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HttpClientHiltModule {
+    @Provides
+    @Singleton
+    @HttpClientGeneric
+    fun providesGenericHttpClient(
+        resources: Resources,
+        subjectTokenRepository: SubjectTokenRepository,
+    ): GenericHttpClient =
+        createHttpClient(
+            subjectTokenRepository = subjectTokenRepository,
+            resources = resources,
+        )
+}
