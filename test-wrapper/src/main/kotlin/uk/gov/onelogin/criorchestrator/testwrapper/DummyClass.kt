2211bf5a0369b4eb04aa000961ee3799d4611d57
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2026-09-14 15:38:39 +0100

    chore: remove dummy class and test

--- test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/DummyClass.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.onelogin.criorchestrator.testwrapper
-
-/** Class that solely exists to provide a matching test that triggers the
- * sonar code coverage calculation **/
-class DummyClass {
-    fun double(value: Int): Int = value.times(DOUBLE)
-}
-
-private const val DOUBLE = 2
