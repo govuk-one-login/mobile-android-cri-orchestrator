eda538c435da9dd47521ff5c0f8ae4c0753d72d4
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2026-09-14 15:06:41 +0100

    fix: lint issue

--- test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/DummyClass.kt
+++ test-wrapper/src/main/kotlin/uk/gov/onelogin/criorchestrator/testwrapper/DummyClass.kt
@@ -3,9 +3,7 @@ package uk.gov.onelogin.criorchestrator.testwrapper
 /** Class that solely exists to provide a matching test that triggers the
  * sonar code coverage calculation **/
 class DummyClass {
-    fun double(value: Int): Int {
-        return value.times(DOUBLE)
-    }
+    fun double(value: Int): Int = value.times(DOUBLE)
 }
 
 private const val DOUBLE = 2
